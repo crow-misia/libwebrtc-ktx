d0fa8d3500d29ae51cf8d522c75ba4f0a44e122d
Zenichi Amano <crow.misia@gmail.com>
2020-11-22 11:49:49 +0900

    libwebrtcをm88.4324.1.0にアップデート (m87にて変更されたJavaAPIに対応)
    各種ライブラリ更新

--- buildSrc/src/main/java/Deps.kt
+++ buildSrc/src/main/java/Deps.kt
@@ -1,4 +1,4 @@
 object Deps {
     const val androidPlugin = "com.android.tools.build:gradle:${Versions.gradlePlugin}"
-    const val webrtc = "com.github.crow-misia:libwebrtc-bin:${Versions.webrtc}"
+    const val webrtc = "com.github.crow-misia:libwebrtc-bin:${Versions.webrtc}@aar"
 }
--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -1,14 +1,14 @@
 object Versions {
-    const val kotlin = "1.3.72"
-    const val gradlePlugin = "4.0.1"
+    const val kotlin = "1.4.20"
+    const val gradlePlugin = "4.1.1"
     const val bintrayPlugin = "1.8.5"
 
-    const val ndk = "21.1.6352462"
-    const val buildTools = "30.0.1"
-    const val compileSdk = 29
+    const val ndk = "21.3.6528147"
+    const val buildTools = "30.0.2"
+    const val compileSdk = 30
     const val minSdk = 16
 
-    const val core = "0.0.2"
+    const val core = "0.1.0"
 
     const val webrtc = "85.4183.2.3"
 }
