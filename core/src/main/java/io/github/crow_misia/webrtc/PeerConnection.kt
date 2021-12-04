bc067c91939dd2990cc6fe51f11692413a911527
Zenichi Amano <crow.misia@gmail.com>
2021-12-04 15:36:21 +0900

    PeerConnectionにStatsを取得するsuspend関数追加

--- core/src/main/java/io/github/crow_misia/webrtc/PeerConnection.kt
+++ core/src/main/java/io/github/crow_misia/webrtc/PeerConnection.kt
@@ -4,9 +4,12 @@ package io.github.crow_misia.webrtc
 
 import io.github.crow_misia.webrtc.observer.CreateSdpObserver
 import io.github.crow_misia.webrtc.observer.SetSdpObserver
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.webrtc.MediaConstraints
 import org.webrtc.PeerConnection
+import org.webrtc.RTCStatsReport
 import org.webrtc.SessionDescription
+import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
 
 suspend inline fun PeerConnection.setLocalDescription(desc: SessionDescription): SessionDescription {
@@ -34,5 +37,12 @@ suspend inline fun PeerConnection.createAnswer(constraints: MediaConstraints): S
     return suspendCoroutine {
         val observer = CreateSdpObserver(it)
         createAnswer(observer, constraints)
+    }
+}
+
+@ExperimentalCoroutinesApi
+suspend inline fun PeerConnection.getStats(): RTCStatsReport {
+    return suspendCoroutine {
+        getStats { report -> it.resume(report) }
     }
 }
