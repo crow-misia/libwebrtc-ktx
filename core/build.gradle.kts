d992c303a02e080feb3e84a5f9fdb94674d4c098
Zenichi Amano <crow.misia@gmail.com>
2022-12-13 19:13:21 +0900

    v1.7.0リリース

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -13,7 +13,7 @@ object Maven {
     const val groupId = "io.github.crow-misia.libwebrtc"
     const val artifactId = "libwebrtc-ktx"
     const val desc = "Libwebrtc Kotlin Extensions"
-    const val version = "1.6.0"
+    const val version = "1.7.0"
     const val siteUrl = "https://github.com/crow-misia/libwebrtc-ktx"
     const val issueTrackerUrl = "https://github.com/crow-misia/libwebrtc-ktx/issues"
     const val gitUrl = "https://github.com/crow-misia/libwebrtc-ktx.git"
