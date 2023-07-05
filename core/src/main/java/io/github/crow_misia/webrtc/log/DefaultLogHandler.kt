0ccab3eead256a0b5248964fe7dcd40f2f551e4b
Zenichi Amano <2736571+crow-misia@users.noreply.github.com>
2023-07-06 08:20:47 +0900

    引数のないログ出力時のメッセージに%が含まれるとクラッシュするバグ修正

--- core/src/main/java/io/github/crow_misia/webrtc/log/DefaultLogHandler.kt
+++ core/src/main/java/io/github/crow_misia/webrtc/log/DefaultLogHandler.kt
@@ -5,17 +5,18 @@ import android.util.Log
 object DefaultLogHandler : LogHandler {
     override fun log(priority: Int, tag: String?, t: Throwable?, message: String?, vararg args: Any?) {
         var msg = message
-        msg = if (msg.isNullOrEmpty()) {
+        if (msg.isNullOrEmpty()) {
             if (t == null) {
                 return
             }
-            Log.getStackTraceString(t)
+            msg = Log.getStackTraceString(t)
         } else {
             val stackTrace = Log.getStackTraceString(t)
+            if (args.isNotEmpty()) {
+                msg = msg.format(*args)
+            }
             if (stackTrace.isEmpty()) {
-                msg.format(*args)
-            } else {
-                "${msg.format(*args)}\n$stackTrace"
+                msg = "${msg}\n$stackTrace"
             }
         }
         Log.println(priority, tag, msg)
