73b9ca8d990f0d782e1af8a1b226d5b63f09f393
Zenichi Amano <2736571+crow-misia@users.noreply.github.com>
2023-10-14 10:15:07 +0900

    fix: h.264のサポート判定を任意のVideoEncoder/DecoderFactoryに対して行えるよう修正

--- core/src/main/java/io/github/crow_misia/webrtc/utils/CodecUtils.kt
+++ core/src/main/java/io/github/crow_misia/webrtc/utils/CodecUtils.kt
@@ -4,20 +4,25 @@ package io.github.crow_misia.webrtc.utils
 
 import org.webrtc.HardwareVideoDecoderFactory
 import org.webrtc.HardwareVideoEncoderFactory
+import org.webrtc.VideoDecoderFactory
+import org.webrtc.VideoEncoderFactory
 
 /**
  * check H.264 Support.
  *
+ * @param videoEncoderFactory Video Encoder Factory
+ * @param videoDecoderFactory Video Decoder Factory
  * @return If true, H.264 supported.
  */
-fun isH264Supported(): Boolean {
-    val hardwareVideoEncoderFactory = HardwareVideoEncoderFactory(null, true, true)
-    val hardwareVideoDecoderFactory = HardwareVideoDecoderFactory(null)
-
-    val h264EncoderSupported = hardwareVideoEncoderFactory.supportedCodecs.any {
+@JvmOverloads
+fun isH264Supported(
+    videoEncoderFactory: VideoEncoderFactory = HardwareVideoEncoderFactory(null, true, true),
+    videoDecoderFactory: VideoDecoderFactory = HardwareVideoDecoderFactory(null),
+): Boolean {
+    val h264EncoderSupported = videoEncoderFactory.supportedCodecs.any {
         it.name.equals("h264", ignoreCase = true)
     }
-    val h264DecoderSupported = hardwareVideoDecoderFactory.supportedCodecs.any {
+    val h264DecoderSupported = videoDecoderFactory.supportedCodecs.any {
         it.name.equals("h264", ignoreCase = true)
     }
 
