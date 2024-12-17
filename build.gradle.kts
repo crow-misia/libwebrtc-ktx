0dce56a686ff26dabf731719ed41a8b646493368
Zenichi Amano <crow.misia@gmail.com>
2024-12-17 16:51:26 +0900

    build: dokka v2対応
    Signed-off-by: Zenichi Amano <crow.misia@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,8 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
-    alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.detekt) apply false
     alias(libs.plugins.dokka) apply false
+    alias(libs.plugins.dokka.javadoc) apply false
+    alias(libs.plugins.kotlin.android) apply false
 }
