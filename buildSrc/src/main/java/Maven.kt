3d96c125dfeaf45565053ad2f2707336a492acad
Zenichi Amano <crow.misia@gmail.com>
2025-01-08 17:45:51 +0900

    chore: bump version 1.10.0 -> 1.11.0

--- buildSrc/src/main/java/Maven.kt
+++ buildSrc/src/main/java/Maven.kt
@@ -2,7 +2,7 @@ object Maven {
     const val GROUP_ID = "io.github.crow-misia.libwebrtc"
     const val ARTIFACT_ID = "libwebrtc-ktx"
     const val DESCRIPTION = "Libwebrtc Kotlin Extensions"
-    const val VERSION = "1.10.0"
+    const val VERSION = "1.11.0"
     const val GITHUB_REPOSITORY = "crow-misia/libwebrtc-ktx"
     const val LICENSE_NAME = "The Apache Software License, Version 2.0"
     const val LICENSE_URL = "http://www.apache.org/licenses/LICENSE-2.0.txt"
